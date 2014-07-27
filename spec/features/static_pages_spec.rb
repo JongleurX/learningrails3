65dcdf9106ca9fbfd3588cf79a297d269cbcd618
Jeremy Bailey <JongleurX@gmail.com>
2014-07-27 10:09:51 -0700

    Wrote view tests; failing

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -5,38 +5,55 @@ require 'rails_helper'
 # /static_pages/home in a browser. 
 # 
 # Uses the page variable (also provided by Capybara) to test that
-# the resulting page has the right content.  
+# the resulting page has the right content.
+#
+# This uses the have_selector method, which checks for an HTML element
+# (the “selector”) with the given content. In other words, this makes
+# sure that the <title> tag is the value specified. 
+
+require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
-  
+
   describe "Help page" do
-    it "should have the content 'Help'" do
+
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_content('Help')
+      expect(page).to have_selector('h1', :text => 'Help')
     end
-  end  
-  
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
   describe "About page" do
-    it "should have the content 'About Us'" do
+
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About Us')
+      expect(page).to have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_selector('title',
+                    :text => "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
-
-# require 'rails_helper'
-#
-# RSpec.describe "StaticPages", :type => :request do
-#   describe "GET /static_pages" do
-#     it "works! (now write some real specs)" do
-#       get static_pages_index_path
-#       expect(response.status).to be(200)
-#     end
-#   end
-# end
