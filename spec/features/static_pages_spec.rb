628226cc9fdfeeba5f823972e43c4df8d4b5cc6e
Jeremy Bailey <JongleurX@gmail.com>
2014-07-27 12:15:04 -0700

    Adding new contact static page, with tests

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -52,4 +52,18 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_title "Ruby on Rails Tutorial Sample App | About Us"    end
   end
+
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title "Ruby on Rails Tutorial Sample App | Contact"    end
+  end
+
 end
