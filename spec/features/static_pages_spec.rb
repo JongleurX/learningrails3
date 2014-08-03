4c0d025393938d8e1ab0133331657fc6b5dd523a
Jeremy Bailey <JongleurX@gmail.com>
2014-08-02 23:10:14 -0700

    Adding shared examples to functional tests

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -17,33 +17,42 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { expect(page).to have_selector('h1',    text: heading) }
+    it { expect(page).to have_title full_title(page_title) }
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { expect(page).to have_selector('h1',    text: 'Sample App') }
-    it { expect(page).to have_title full_title('') }
+    it_should_behave_like "all static pages"
     it { expect(page).to_not have_title '| Home' }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)    { 'Help' }
+    let(:page_title) { 'Help' }
 
-    it { expect(page).to have_selector('h1',    text: 'Help') }
-    it { expect(page).to have_title full_title('Help') }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About Us' }
 
-    it { expect(page).to have_selector('h1',    text: 'About') }
-    it { expect(page).to have_title full_title('About Us') }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)    { 'Contact' }
+    let(:page_title) { 'Contact' }
 
-    it { expect(page).to have_selector('h1',    text: 'Contact') }
-    it { expect(page).to have_title full_title('Contact') }
+    it_should_behave_like "all static pages"
   end
 end
 
