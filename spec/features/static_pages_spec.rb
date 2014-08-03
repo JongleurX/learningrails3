61ba5634b877436921b14c3d3b08c2d767871125
Jeremy Bailey <JongleurX@gmail.com>
2014-08-02 23:33:46 -0700

    Adding link tests for homepage

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -18,8 +18,11 @@ describe "Static pages" do
   subject { page }
 
   shared_examples_for "all static pages" do
+ 
+    # Page content tests
     it { expect(page).to have_selector('h1',    text: heading) }
     it { expect(page).to have_title full_title(page_title) }
+
   end
 
   describe "Home page" do
@@ -29,6 +32,22 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it { expect(page).to_not have_title '| Home' }
+
+    # Link destination tests
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title full_title('About Us')
+      click_link "Help"
+      expect(page).to have_title full_title('Help')
+      click_link "Contact"
+      expect(page).to have_title full_title('Contact')
+      click_link "Home"
+      click_link "Sign up now!"
+      expect(page).to have_title full_title('Sign up')
+      click_link "sample app"
+      expect(page).to have_title full_title('')
+    end
   end
 
   describe "Help page" do
